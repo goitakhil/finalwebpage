ebe0c8ccf0fe04c5a474079f4ae39bbc1141adb8
goitakhil <akhileshgoit@u.boisestate.edu>
2019-11-06 17:27:49 -0700

    Final edit

---
+++ allgallery.php
@@ -0,0 +1,28 @@
+<?php
+  include_once 'header.php';
+ ?>
+ <!-- Images Gallery -->
+ <div class="container">
+   <img src="image/IMG_0280.jpg">
+   <img src="image/IMG_1659c.jpg">
+   <img src="image/IMG_1646.jpg">
+   <img src="image/IMG_1647.jpg">
+   <img src="image/IMG_1657.jpg">
+   <img src="image/IMG_1651.jpg">
+   <!-- <img src="image/IMG_1653.jpg"> -->
+   <img src="image/IMG_1653.jpg">
+   <img src="image/IMG_1655.jpg">
+   <img src="image/IMG_1654.jpg">
+   <img src="image/IMG_1669.jpg">
+   <img src="image/IMG_1667.jpg">
+   <img src="image/IMG_1665.jpg">
+   <img src="image/IMG_1664.jpg">
+   <img src="image/IMG_1663.jpg">
+   <img src="image/IMG_1660.jpg">
+
+
+ </div>
+
+ <?php
+   include_once 'footer.php';
+  ?>
